ddd3cc56e464213a14984ff8614dcae756195d51
Malte Mindedal <malte.mindedal@gmail.com>
2024-11-12 20:20:14 +0100

    Update formatting

--- src/http_cli/exceptions.py
+++ src/http_cli/exceptions.py
@@ -1,11 +1,16 @@
 class HTTPClientError(Exception):
     """Base exception for HTTP client errors"""
+
     pass
+
 
 class ConnectionError(HTTPClientError):
     """Raised when connection fails"""
+
     pass
+
 
 class ResponseError(HTTPClientError):
     """Raised when response is invalid"""
+
     pass
