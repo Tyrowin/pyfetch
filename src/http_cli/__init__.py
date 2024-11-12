fce28e77c13d0517cd54d928b4a95761d9d7eb29
Malte <112257731+Tyrowin@users.noreply.github.com>
2024-11-12 20:20:14 +0100

    Update formatting

--- src/http_cli/__init__.py
+++ src/http_cli/__init__.py
@@ -1,4 +1,4 @@
 from http_cli.http_client import HTTPClient
 from http_cli.exceptions import HTTPClientError, ConnectionError, ResponseError
 
-__all__ = ['HTTPClient', 'HTTPClientError', 'ConnectionError', 'ResponseError']
+__all__ = ["HTTPClient", "HTTPClientError", "ConnectionError", "ResponseError"]
