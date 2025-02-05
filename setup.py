3160ea947879a1213a4869b1b97c85723fe41511
Malte <112257731+Tyrowin@users.noreply.github.com>
2025-02-05 19:52:53 +0100

    Update README and setup.py for repository name change to PyFetch

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="PyFetch",  # Changed from http-cli
+    name="PyFetch",
     version="1.0.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
@@ -12,7 +12,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "pyfetch=http_cli.cli:main",  # Changed from http_cli=http_cli.cli:main
+            "pyfetch=http_cli.cli:main",
         ],
     },
     author="Malte Mindedal",
